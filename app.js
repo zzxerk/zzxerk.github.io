b70eb6298e4a0bbd575d3e1248a51f5dd3cbcd7b
zzxerk <ktrprkjax@gmail.com>
2024-06-15 23:17:57 +0300

    new

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ tg.expand();
 let out = document.querySelector(".user__out");
 out.insertAdjacentHTML("beforeend", 
     `
-    <p class="user__name">${tg.initDataUnsafe.user.id}</p>
+    <p class="user__name">${tg.initDataUnsafe.user.first_name}</p>
+    <p class="user__id">${tg.initDataUnsafe.user.id}</p>
     `
 )
 
