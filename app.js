f00e0e12d148eb6fda4b25a8c6a434be0665e925
zzxerk <ktrprkjax@gmail.com>
2024-06-16 01:14:00 +0300

    commit

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
+let count = document.querySelector('.count');
+let c = 0;
+let btn = document.querySelector('.btn')
+btn.addEventListener('click', function () {
+    c++;
+    count.textContent = c;
+})
+
+
 
 let out = document.querySelector(".user__out");
 out.insertAdjacentHTML("beforeend", 
     `
     <p class="user__name">@${tg.initDataUnsafe.user.username}</p>
-    <p class="user__id">${tg.initDataUnsafe.user.id}</p>
-    <p>${tg.initDataUnsafe.user.photo_url}</p>
     `
-)
+);
 
-// let userName = document.querySelector(".user__name");
 
-// userName.innerText = `${tg.initDataUnsafe.user.first_name}`;
+
+
+// <p class="user__id">${tg.initDataUnsafe.user.id}</p>
