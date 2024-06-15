8a446e7934322885e542d8dee125be77c83274d1
zzxerk <ktrprkjax@gmail.com>
2024-06-15 22:57:07 +0300

    new

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 
 
-let out = document.querySelector(".out");
+let out = document.querySelector(".user__out");
 out.insertAdjacentHTML("beforeend", 
     `
-    <p class="user__name">${tg.initDataUnsafe.user.first_name}</p>
+    <p class="user__name">${tg.initDataUnsafe.user.username}</p>\
+    <p class="user__name">${tg.initDataUnsafe.user.id}</p>
     `
 )
 
