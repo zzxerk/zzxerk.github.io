24c9b3b7a1d17983e37d6f65e7f40748516e4022
zzxerk <ktrprkjax@gmail.com>
2024-06-15 22:27:34 +0300

    new

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
-tg.platform;
+
 
 let out = document.querySelector(".out");
 out.insertAdjacentHTML("beforeend", 
     `
     <p class="user__name">${tg.initDataUnsafe.user.first_name}</p>
     `
-);
+)
 
 // let userName = document.querySelector(".user__name");
 
