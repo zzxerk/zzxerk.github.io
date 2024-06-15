fc159ca84012b232fb4f885ec7cc486aabcde58f
zzxerk <ktrprkjax@gmail.com>
2024-06-15 21:46:20 +0300

    commit

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-let ppp = document.querySelector(".ppp");
-ppp.innerText = "ppp"
+let out = document.querySelector(".out");
+out.insertAdjacentHTML("beforeend", 
+    `
+    <p class="user__name">${tg.initDataUnsafe.user.first_name}</p>
+    `
+)
 
-let userName = document.querySelector(".user__name");
+// let userName = document.querySelector(".user__name");
 
-userName.innerText = `${tg.initDataUnsafe.user.first_name}`;
+// userName.innerText = `${tg.initDataUnsafe.user.first_name}`;
