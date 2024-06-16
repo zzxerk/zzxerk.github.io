8ad54c7cbb75824e1b63f9205b17ac83dd5bffce
zzxerk <ktrprkjax@gmail.com>
2024-06-16 21:36:57 +0300

    commit

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ tg.expand();
 
 
 let c = 0;
+
+let tap = 10;
+
 const saveCount = () => {
     window.localStorage.setItem('count', c)
 }
@@ -12,23 +15,24 @@ let count = document.querySelector('.count');
 let btn = document.querySelector('.btn');
 
 const loadCount = () => {
+    c.toLocaleString('ru')
     c = (window.localStorage.getItem('count'))? window.localStorage.getItem('count'): 0;
-    count.textContent = c;
+    count.textContent = c.toLocaleString('en');
 }
 btn.addEventListener('click', function () {
     c++;
-    count.textContent = c;
+    count.textContent = c.toLocaleString('en');
     saveCount()
 })
 
 loadCount()
 
 
-let out = document.querySelector(".user__out");
-out.insertAdjacentHTML("beforeend", 
-    `
-    <p class="user__name">@${tg.initDataUnsafe.user.username}</p>
-    `
-);
+// let out = document.querySelector(".user__out");
+// out.insertAdjacentHTML("beforeend", 
+//     `
+//     <p class="user__name">@${tg.initDataUnsafe.user.username}</p>
+//     `
+// );
 
-// <p class="user__id">${tg.initDataUnsafe.user.id}</p>
+// // <p class="user__id">${tg.initDataUnsafe.user.id}</p>
