a9bf0b92d80169948c966a3309aef42a44a43217
zzxerk <ktrprkjax@gmail.com>
2024-06-17 01:28:29 +0300

    commit

--- app.js
+++ app.js
@@ -9,19 +9,22 @@ p = energyLimit;
 let c = 0;
 const saveCount = () => {
     window.localStorage.setItem('count', c)
+    window.localStorage.setItem('energy', energyLimit)
 }
 
 let count = document.querySelector('.count');
 let btn = document.querySelector('.btn');
+let energyCount = document.querySelector('.energy__count');
 
 const loadCount = () => {
     c = (window.localStorage.getItem('count'))? window.localStorage.getItem('count'): 0;
     count.textContent = c;
+    energyLimit = (window.localStorage.getItem('energy'))?window.localStorage.getItem('energy'): p;
+    energyCount.textContent = energyLimit+"/"+p;
 }
 btn.addEventListener('click', function () {
     if (energyLimit > 0) {
         energyLimit--;
-        let energyCount = document.querySelector('.energy__count');
         energyCount.textContent = energyLimit+"/"+p;
         c++;
         count.textContent = c;
