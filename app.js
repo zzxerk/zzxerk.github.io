e4c68612cf58705676cc45562c30d3d8fb8c90b4
zzxerk <ktrprkjax@gmail.com>
2024-06-16 21:38:40 +0300

    commit

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ btn.addEventListener('click', function () {
 loadCount()
 
 
-// let out = document.querySelector(".user__out");
-// out.insertAdjacentHTML("beforeend", 
-//     `
-//     <p class="user__name">@${tg.initDataUnsafe.user.username}</p>
-//     `
-// );
+let out = document.querySelector(".user__out");
+out.insertAdjacentHTML("beforeend", 
+    `
+    <p class="user__name">@${tg.initDataUnsafe.user.username}</p>
+    `
+);
 
-// // <p class="user__id">${tg.initDataUnsafe.user.id}</p>
+// <p class="user__id">${tg.initDataUnsafe.user.id}</p>
