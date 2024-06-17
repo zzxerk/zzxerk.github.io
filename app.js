bf1e99de42a1a5d0b5ae3897754ab1f1a6e50127
zzxerk <ktrprkjax@gmail.com>
2024-06-17 23:04:06 +0300

    commit

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ tg.expand();
 let energyLimit = 100;
 let p = energyLimit;
 
-
+let time = new Date().getTime();
+console.log(time);
 
 let c = 0;
+
 const saveCount = () => {
     window.localStorage.setItem('count', c)
     window.localStorage.setItem('energy', energyLimit)
@@ -18,6 +20,7 @@ const saveCount = () => {
 let count = document.querySelector('.count');
 let btn = document.querySelector('.btn');
 let energyCount = document.querySelector('.energy__count');
+let freeEnergy = document.querySelector('.free__energy');
 
 const loadCount = () => {
     c = (window.localStorage.getItem('count'))? window.localStorage.getItem('count'): 0;
@@ -25,45 +28,55 @@ const loadCount = () => {
     energyLimit = (window.localStorage.getItem('energy'))?window.localStorage.getItem('energy'): p;
     energyCount.textContent = energyLimit+"/"+p;
     window.localStorage.getItem('maxEnergy')
+    window.localStorage.getItem('nextTime')
 }
 
+function a () {
+    if (energyLimit==0) {
+    freeEnergy.classList.add("active");
+}
+}
+
+a()
 
 btn.addEventListener('click', function () {
-
+    // let nowTime = new Date().getTime();
+    // let nextTime = nowTime+60000;
     if (energyLimit > 0) {
         energyLimit--;
-        let energyTime = (p-energyLimit)*1000;
         energyCount.textContent = energyLimit+"/"+p;
         c++;
         count.textContent = c;
-        let energyInterval = setInterval(function () {
-            if (energyLimit <= p) {
-                energyLimit++;
-                energyCount.textContent = energyLimit+"/"+p;            
-                clearInterval(energyInterval);
-            // console.log((p-energyLimit-1)*2000);
-            }
-        }, energyTime);
     }
-    saveCount()
+    a();
+    saveCount();
+})
+
+
+freeEnergy.addEventListener('click', function () {
+    if (energyLimit==0) {
+        energyLimit=100;
+        energyCount.textContent = energyLimit+"/"+p;
+        freeEnergy.classList.remove("active");
+    }
+    saveCount();
 })
 
 loadCount()
 
-// let energyInterval = setInterval(function () {
-//     if (window.localStorage.getItem('energy')<p) {
-//         energyLimit++;
-//         saveCount;
-//         energyCount.textContent = energyLimit+"/"+p;
-//         clearInterval(energyInterval);
-//     }
-// }, 2000)
+// let out = document.querySelector(".user__out");
+// out.insertAdjacentHTML("beforeend", 
+//     `
+//     <p class="user__name">@${tg.initDataUnsafe.user.username}</p>
+//     `
+// );
 
-let out = document.querySelector(".user__out");
-out.insertAdjacentHTML("beforeend", 
-    `
-    <p class="user__name">@${tg.initDataUnsafe.user.username}</p>
-    `
-);
 
-// <p class="user__id">${tg.initDataUnsafe.user.id}</p>
+// let energyTime = (p-energyLimit)*200;
+        // let energyInterval = setInterval(function () {
+        //     if (energyLimit<=p) {
+        //         energyLimit++;
+        //         energyCount.textContent = energyLimit+"/"+p;            
+        //         clearInterval(energyInterval);
+        //     }
+        // }, energyTime);
